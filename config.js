b3c31408d510579ba51cb437d9eda090354616c1
PORMODKANFAI <unongnongt@gmail.com>
2026-05-09 15:28:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   // ตั้งค่า API URL ที่นี่
 window.APP_CONFIG = {
   // วาง Google Apps Script Web App URL ที่นี่
-  API_URL: 'https://script.google.com/macros/s/AKfycbz-98xPFBjiUZ4G_7SuJrYDnJ9twKtT4gtaRjbdlLkdIjfOd0jAEnt7GExFCW5PVRn8/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyYfMPAe49pvLU8H0n2xqTF0i3xyk9Ear36RkFf96l1tayxQ8m6gVlJ8uFQlobkZ7Y/exec',
   
   APP_NAME: 'ระบบเบิกจ่ายวัสดุ',
   VERSION:  '1.0',
